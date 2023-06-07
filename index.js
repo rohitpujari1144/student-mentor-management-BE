65af581a4b03fb5c91e2709290df8872ce719ae5
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-06-07 22:40:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { MongoClient, ObjectId } = require('mongodb')
 const mongodb = require('mongodb')
-const cors=require('cors')
+const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -65,12 +65,52 @@ app.post('/createStudent', async (req, res) => {
     }
 })
 
+// getting all students with no mentors
+app.get('/getAllStudents', async (req, res) => {
+    const client = await MongoClient.connect(dbUrl);
+    try {
+        const db = await client.db("Student_Mentor_Management");
+        let mentor = await db.collection("studentData").find({ assignedMentor: "" }).toArray()
+        if (mentor.length == 0) {
+            res.status(404).send({ message: 'No student created yet !' })
+        }
+        else {
+            res.status(200).send({ data: mentor })
+        }
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).send({ message: 'Internal server error', error })
+    }
+    finally {
+        client.close()
+    }
+})
+
+// assigning mentor to student
+app.put('/assignMentor/:studentEmail', async (req, res) => {
+    const client = await MongoClient.connect(dbUrl);
+    try {
+        const db = await client.db("Student_Mentor_Management");
+        await db.collection("studentData").updateOne({ emailId: req.params.studentEmail }, {$set: req.body})
+        res.status(200).send({ message: 'Mentor assigned to student !' })
+
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).send({ message: 'Internal server error', error })
+    }
+    finally {
+        client.close()
+    }
+})
+
 // getting all students of a particular mentor
 app.get('/getAllStudents/:mentorName', async (req, res) => {
     const client = await MongoClient.connect(dbUrl);
     try {
         const db = await client.db("Student_Mentor_Management");
-        let student = await db.collection("studentData").find({assignedMentor:req.params.mentorName}).toArray()
+        let student = await db.collection("studentData").find({ assignedMentor: req.params.mentorName }).toArray()
         if (student.length == 0) {
             res.status(404).send({ message: 'No student assigned to mentor yet !' })
         }
